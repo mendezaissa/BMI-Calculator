a6f3c225e58fe860fd6997ca316ef2848e22dfcb
Aissa Mendez <32945225+mendezaissa@users.noreply.github.com>
2020-07-02 21:59:46 -0500

    Add files via upload

--- bmi calculator/bmi.js
+++ bmi calculator/bmi.js
@@ -21,18 +21,56 @@
 
 })();
 
-//calculate bmi
+var result = "";
+
+//calculate bmi function
 function calculateBmi()
 {
   //retrieves the value of id called pounds and stores it in variable 'pounds'
-  var pounds = document.getElementById('pounds').value;
+  var lbs = document.getElementById('pounds').value;
 
   //retrieves the value of id called inches and stores it in variable 'inches'
-  var inches = document.getElementById('inches').value;
+  var inch = document.getElementById('inches').value;
 
   //retrieves the value of id called feet and stores it in variable 'feet'
-  var feet = document.getElementById('feet').value;
+  var ft = document.getElementById('feet').value;
 
-  //convert to integers for computation
-  
+  //convert to integers
+  var weight = parseInt(lbs);
+  var inches = parseInt(inch);
+  var ft = parseInt(ft);
+
+  //converts # of feet to inches then adds to inches
+  var height = (ft * 12) + inches;
+
+  //calculates bmi
+  var bmi = (weight / ( height*height )) * 703;
+
+  //truncates bmi down to one decimal point
+  bmi = parseFloat(bmi.toFixed(1));
+  console.log(bmi);
+
+  //based on calculated bmi, result is assigned a value
+  if(bmi < 18.5)
+  {
+    result = "Underweight";
+  }
+  else if(bmi >= 18.5 && bmi <= 24.9)
+  {
+    result = "Normal";
+  }
+  else if(bmi >= 25.0 && bmi <= 29.9)
+  {
+    result = "Overweight";
+  }
+  else if(bmi >= 30.0)
+  {
+    result = "Obese";
+  }
+  else
+  {
+    result = "Please check the accuracy of the information you entered. Try again."
+  }
+
+  alert(result);
 }
